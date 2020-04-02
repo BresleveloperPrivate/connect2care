87b705779644988f1afce51415184b63c13736b8
maayanLCohen <maayan45633@gmail.com>
2020-04-02 22:49:42 +0300

    facebook

--- src/components/Sharing.jsx
+++ src/components/Sharing.jsx
@@ -158,10 +158,10 @@ export default function Sharing(props) {
       display: 'popup',
       action_properties: JSON.stringify({
         object: {
-          'og:url': 'https://lohamim.carmel6000.com/#/',
-          'og:title': 'crap in pita',
-          'og:description': 'not tasty',
-          'og:image': 'http://izkorcdn.azureedge.net/Data/korot/Image/506173.jpg'
+          'og:url': 'https://lohamim.carmel6000.com/#/?og_img=http://izkorcdn.azureedge.net/Data/korot/Image/506173.jpg',
+          // 'og:title': 'crap in pita',
+          // 'og:description': 'not tasty',
+          // 'og:image': 'http://izkorcdn.azureedge.net/Data/korot/Image/506173.jpg'
         }
       })
     }, function(response) {
