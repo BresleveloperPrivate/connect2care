21096b01e4e99fc5cdf4b8cfce24eb557d67ad93
noam-moshe <noamoshe1234@gmail.com>
2020-04-05 19:16:51 +0300

    language

--- src/components/Language.js
+++ src/components/Language.js
@@ -4,11 +4,11 @@
 
 import React, { useState, useRef } from 'react'
 import '../styles/animations.scss'
+import { inject, observer } from 'mobx-react';
 // import DownArrow from '../icons/Icon awesome-chevron-down.svg'
 import useOnClickOutside from './UseOnClickOutside'
 import '../styles/select.css'
-import heb from '../icons/heb.svg'
-import en from '../icons/en.svg'
+
 const useForceUpdate = () => useState()[1];
 const Language = (props) => {
     const forceUpdate = useForceUpdate();
@@ -21,24 +21,15 @@ const Language = (props) => {
     // };
 
     // setOptions = () => {
-    const Options =
-        [
-            { option: 'עברית', short: 'heb', img: heb },
-            { option: 'English', short: 'en', img: en },
-            { option: 'עברית', short: 'heb', img: heb },
-            { option: 'עברית', short: 'heb', img: heb },
-            { option: 'עברית', short: 'heb', img: heb },
-        ]
+    
     // }
 
 
 
     const ref = useRef()
+
     useOnClickOutside(ref, () => setIsSelectOpen(false));
-
-    const currentLang = Options.findIndex(val => val.short === localStorage.getItem('lang'))
-
-    const [selectedIndex, setSelectedIndex] = useState(currentLang)
+    
     const [isSelectOpen, setIsSelectOpen] = useState(false)
 
     return (
@@ -47,21 +38,21 @@ const Language = (props) => {
             <div className='selectContainer' style={{ width: '90px' , fontSize:'0.8em' }} >
                 <div className='d-flex select align-items-center h-100'>
                     <div className="selectInput" style={{ width: '100%', display: 'flex', alignItems: 'center', cursor: 'pointer', padding: '1vh 2vh', color: props.mode2 ? 'white' : null }}>
-                        <div style={{ height: '1.2em', width: '1.2em', marginLeft: '0.4em', display: 'flex' }}><img style={{ borderRadius: '50%', objectFit: 'cover' }} height='100%' width='100%' src={Options[selectedIndex].img} /></div>
-                        {Options[selectedIndex].option}
+                        <div style={{ height: '1.2em', width: '1.2em', marginLeft: '0.4em', display: 'flex' }}><img style={{ borderRadius: '50%', objectFit: 'cover' }} height='100%' width='100%' src={props.LanguageStore.Options[props.LanguageStore.selectedIndex].img} /></div>
+                        {props.LanguageStore.Options[props.LanguageStore.selectedIndex].option}
                     </div>
                     {/* <img style={{ marginLeft: '1vw' }} className="arrowInput" src={DownArrow} alt='arrow' /> */}
                 </div>
                 {isSelectOpen &&
                     <div className='optionsContainer' style={{ backgroundColor: props.backgroundColor || 'white', color: props.color || 'unset' }}>
-                        {Options.map((value, index) =>
+                        {props.LanguageStore.Options.map((value, index) =>
                             <div style={{ cursor: 'pointer' }}
                                 className='selectOption'
                                 key={index}
                                 onClick={() => {
                                     props.changeLanguage(value.short)
                                     //   this.setOptions()
-                                    setSelectedIndex(index)
+                                    props.LanguageStore.setSelectedIndex(index)
                                     // forceUpdate()
                                 }}>
                                 <div style={{ display: 'flex', alignItems: 'center' }} className='optionInSelect'>
@@ -77,4 +68,4 @@ const Language = (props) => {
     )
 }
 
-export default Language;
+export default inject('LanguageStore')(observer(Language))
