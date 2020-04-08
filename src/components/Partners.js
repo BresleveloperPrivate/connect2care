04cfdcb925f535d7115e8f71f1f0f55c8534bc7f
noam-moshe <noamoshe1234@gmail.com>
2020-04-08 13:13:04 +0300

    partners

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -6,6 +6,16 @@ import amit from '../icons/amit.png'
 import facebook from '../icons/facebook.png'
 import synamedia from '../icons/Synamedia.png'
 
+import hilmaWhite from '../icons/hilmaWhite.png'
+import logo1 from '../icons/logo1.jpg'
+import logo2 from '../icons/logo2.png'
+import logo3 from '../icons/logo3.png'
+import logo4 from '../icons/logo4.jpg'
+import logo5 from '../icons/logo5.png'
+import logo6 from '../icons/logo6.png'
+import logo7 from '../icons/logo7.PNG'
+import logo8 from '../icons/logo8.png'
+
 
 class Partners extends Component {
 
@@ -23,13 +33,31 @@ class Partners extends Component {
                 </div>
 
                 <div className='containPartners'>
-                    <div className='partner'><img src={can} height='110%' /></div>
+                    {/* <div className='partner'><img src={can} height='110%' /></div>
                     <div className='partner'><img src={amit} height='110%' /></div>
                     <div className='partner'><img src={synamedia} height='130%' /></div>
                     <div className='partner'><img src={facebook} height='90%' /></div>
+                    <div className='partner'><img src={hilma} height='70%' /></div> */}
                     <div className='partner'><img src={hilma} height='70%' /></div>
+                    <div className='partner'><img src={synamedia} height='150%' /></div>
+                    <div className='partner'><img src={amit} height='110%' /></div>
+                    <div className='partner'><img src={can} height='110%' /></div>
+                    <div className='partner'><img src={logo1} height='100%' /></div>
+                    <div className='partner'><img src={logo2} height='60%' /></div>
+                    <div className='partner'><img src={logo3} height='100%' /></div>
+                    <div className='partner'><img src={logo4} height='100%' /></div>
+                    <div className='partner'><img src={logo5} height='100%' /></div>
+                    <div className='partner'><img src={logo6} height='80%' /></div>
+                    <div className='partner'><img src={logo7} height='100%' /></div>
+                    <div className='partner'><img src={logo8} height='50%' /></div>
+
+
+
 
                 </div>
+                <div className='hilmeCredit'>
+                    האתר פותח כתרומה לחברה ע"י הילמה - הייטק למען החברה
+                  <div style={{height:'1em' ,marginRight:'4vw' , display:'flex'}}><img src={hilmaWhite} height='100%' /></div> </div>
 
             </div>
 
