3b39a6c982bb53f6e7ff34496d89af34dbdbcf22
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-04-12 21:44:12 +0300

    dono

--- src/components/Meeting/MeetingFallen.jsx
+++ src/components/Meeting/MeetingFallen.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { makeStyles, Avatar } from '@material-ui/core';
 
+(async () => {
+    const res = await fetch("https://api.zoom.us/v2/users", {
+        method: "POST",
+        body: JSON.stringify({
+            api_key: "xdUTS0PfRniExY5nSVf01w",
+            api_secret: "m5R5G0HZBFoJDOgBQ3ieqk48W8XK1QFV0Uw8"
+        })
+    });
+    console.log(res)
+})();
+
 const useStyles = makeStyles({
     avatar: {
         height: 167,
@@ -13,7 +24,7 @@ const MeetingFallen = ({ fallen: { name, falling_date, image_link } }) => {
     const { avatar } = useStyles();
     return (
         <div className="meetingFallen">
-            <Avatar src={image_link || "./images/fallenFallback.jpeg"} className={avatar} variant="square" />
+            <Avatar src={image_link || "./images/fallenFallback.jpeg"} className={avatar} variant="rounded" />
             <div className="meetingFallenDescription">
                 <img alt="alt" src="./images/lightBlueCandleIcon.svg" className="fallenCandle" />
                 <div className="fallenName">{`${name || ''}`}</div>
