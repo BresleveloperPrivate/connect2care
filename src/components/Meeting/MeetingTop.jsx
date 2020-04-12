0ac3cdb99c176544d44e2ba48f7c69fb2b74fbd6
noam-moshe <noamoshe1234@gmail.com>
2020-04-12 19:19:22 +0300

    translation

--- src/components/Meeting/MeetingTop.jsx
+++ src/components/Meeting/MeetingTop.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 
 
-const MeetingTop = ({ name, owner, description, date, time ,LanguageStore}) => (
+
+const MeetingTop = ({ name, owner, description, date, time ,LanguageStore , t}) => {
+    const meetingDate = [
+        { option: t('all'), data: false },
+        { option: t('sunday'), data: 'יום ראשון, ב באייר, 26.04' },
+        { option: t('monday'), data: 'יום שני, ג באייר, 27.04' },
+        { option: t('tuesday'), data: 'יום שלישי, ד באייר, 28.04' },
+        { option: t('wednesday'), data: 'יום רביעי, ה באייר, 29.04' },
+    ]
+    
+    return(
     <div style={LanguageStore.lang !== 'heb' ? {alignItems:'flex-end'}:{}} id="meetingTop">
         {name && name.length !== 0 && <div id="meetingName">{name}</div>}
         {owner && owner.length !== 0 && <div id="meetingOwner"> {LanguageStore.lang !== 'heb' ? 'Host' : 'מארח/ת'}: {owner}</div>}
@@ -10,8 +20,12 @@ const MeetingTop = ({ name, owner, description, date, time ,LanguageStore}) => (
         <p style={LanguageStore.lang !== 'heb' ? {textAlign:'left'}:{textAlign:'right'}} id="meetingDescription">
             {description}</p>
             }
-        {((date && date.length !== 0) || (time && time.length !== 0)) && <div id="meetingDateTime">{`${date.split("T")[0]}${date && time && " | "}${time && `בשעה ${time}`}`}</div>}
+        {((date && date.length !== 0) || (time && time.length !== 0)) && <div id="meetingDateTime">
+
+            {t(meetingDate.find(val=> val.data === date).option)} | {LanguageStore.lang !== 'heb' ? 'At' : 'בשעה'} {time}
+            
+            </div>}
     </div>
-);
+)};
 
 export default inject('LanguageStore')(observer(MeetingTop));
