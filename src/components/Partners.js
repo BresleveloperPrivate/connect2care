045d7b090c81abe2529ccb26c49a0ac1bef49425
noam-moshe <noamoshe1234@gmail.com>
2020-04-14 15:27:19 +0300

    שוב הלוגואים האלהההההה נמאס ליייייייייי מה יש להם אלההההההההה

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -6,7 +6,7 @@ import hilma from '../icons/hilma.png'
 import amit from '../icons/amit.png'
 import tobe from '../icons/tobe.png'
 import synamedia from '../icons/Synamedia.png'
-import zoom from '../icons/zoom.png'
+import zoom from '../icons/zoom.svg'
 import hilmaWhite from '../icons/hilmaWhite.png'
 import logo1 from '../icons/logo1.jpg'
 import logo2 from '../icons/logo2.png'
@@ -43,18 +43,18 @@ class Partners extends Component {
                     <div onClick={() => { window.open('http://www.amit.org.il/') }} className='partner'><img src={amit} height='110%' /></div>
                     <div onClick={() => { window.open('https://www.hilma.tech/') }} className='partner'><img src={hilma} height='50%' /></div>
                     <div onClick={() => { window.open('https://www.synamedia.com/') }} className='partner'><img src={synamedia} height='180%' /></div>
-                    <div onClick={() => { window.open('https://grth.io/2bcloud/') }} className='partner'><img src={tobe} height='70%' /></div>
-                    <div onClick={() => { window.open('https://www.bac.org.il/') }} className='partner'><img src={logo3} height='90%' /></div>
-                    <div onClick={() => { window.open('http://www.my-idea.co.il/') }} className='partner'><img src={logo7} height='120%' /></div>
-                    <div onClick={() => { window.open('https://edu.gov.il/noar/minhal/Pages/hp.aspx') }} className='partner'><img src={logo12} height='110%' /></div>
-                    <div onClick={() => { window.open('https://zoom.us/') }} className='partner'><img src={zoom} height='80%' /></div>
+                    <div onClick={() => { window.open('https://grth.io/2bcloud/') }} className='partner'><img src={tobe} height='60%' /></div>
+                    <div onClick={() => { window.open('https://www.bac.org.il/') }} className='partner'><img src={logo3} height='80%' /></div>
+                    <div onClick={() => { window.open('http://www.my-idea.co.il/') }} className='partner'><img src={logo7} height='110%' /></div>
+                    <div onClick={() => { window.open('https://edu.gov.il/noar/minhal/Pages/hp.aspx') }} className='partner'><img src={logo12} height='90%' /></div>
+                    <div onClick={() => { window.open('https://zoom.us/') }} className='partner'><img src={zoom} height='40%' /></div>
                     <div onClick={() => { window.open('https://www.naotech.com/he/') }} className='partner'><img src={nao} height='40%' /></div>
-                    <div onClick={() => { window.open('https://he-il.facebook.com/facebook') }} className='partner'><img src={logo6} height='70%' /></div>
                     <div onClick={() => { window.open('https://digistage.co.il/') }} className='partner'><img src={logo8} height='40%' /></div>
-                    <div onClick={() => { window.open('http://project21.co.il/') }} className='partner'><img src={logo5} height='100%' /></div>
-                    <div onClick={() => { window.open('https://www.schusterman.org/') }} className='partner fg0'><img src={logo4} height='110%' /></div>
-                    <div onClick={() => { window.open('https://www.wework.com/he-IL') }} className='partner fg0'><img src={logo1} height='80%' /></div>
+                    <div onClick={() => { window.open('https://www.schusterman.org/') }} className='partner '><img src={logo4} height='110%' /></div>
+                    <div onClick={() => { window.open('https://he-il.facebook.com/facebook') }} className='partner'><img src={logo6} height='70%' /></div>
                     <div onClick={() => { window.open('https://payboxapp.com/il/home') }} className='partner fg0'><img src={logo2} height='70%' /></div>
+                    <div onClick={() => { window.open('http://project21.co.il/') }} className='partner fg0'><img src={logo5} height='100%' /></div>
+                    <div onClick={() => { window.open('https://www.wework.com/he-IL') }} className='partner fg0'><img src={logo1} height='80%' /></div>
 
                 </div>
                 <div className={this.props.LanguageStore.lang !== 'heb' ? 'hilmeCredit fdrr' : 'hilmeCredit'}>
