9e858705367506386314b4b9dfb13380d09774ef
moriagro <moriagro@gmail.com>
2020-04-19 18:30:36 +0300

    fix problebm hopefuly

--- common/models/meetings.js
+++ common/models/meetings.js
@@ -20,14 +20,9 @@ module.exports = function (meetings) {
 
     meetings.getMeetingsUser = (search, filters, limit, options, cb) => {
         let sqlQuerySelect = `meetings.id`
-<<<<<<< HEAD
-        let sqlQueryfrom = `meetings`
-        let sqlQueryWhere = ``
-=======
         let sqlQueryfrom = `meetings , fallens_meetings`
         let sqlQueryWhere = `meetings.id = fallens_meetings.meeting `
         let params = []
->>>>>>> fbc5d9914e2df6a4b580ee78075915bca706f520
         let searchArr = search.split("'")
         let newSearch = ""
         for (let i = 0; i < searchArr.length; i++) {
