179e15688cbd7045d2c15b88f949ec4c56451c37
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-04-19 10:00:19 +0300

    flat fix

--- server/boot/scheduleBeforeMeetings.js
+++ server/boot/scheduleBeforeMeetings.js
@@ -1,51 +1,53 @@
-// 'use strict';
+'use strict';
 
-// const schedule = require('node-schedule');
+const schedule = require('node-schedule');
 
-// const validateRules = require('../lib/validateRules');
+const validateRules = require('../lib/validateRules');
 
-// module.exports = function (app) {
-//     const dates = validateRules.meetings.date.format.pattern.split("(").join("").split(")").join("").split("^").join("").split("|");
+module.exports = function (app) {
+    const dates = validateRules.meetings.date.format.pattern.split("(").join("").split(")").join("").split("^").join("").split("|");
 
-//     dates.forEach(date => {
+    dates.forEach(date => {
 
-//         const hours = [5, 8, 11, 14, 17, 20, 23];
-//         const minutes = ['00', '30'];
+        const hours = [5, 8, 11, 14, 17, 20, 23];
+        const minutes = ['00', '30'];
 
-//         hours.forEach(hour => {
-//             const nextHour = hour + 3;
-//             const rule = new schedule.RecurrenceRule();
+        hours.forEach(hour => {
+            const nextHour = hour + 3;
+            const rule = new schedule.RecurrenceRule();
 
-//             const nextHours = [];
+            const nextHours = [];
 
-//             for (let index = nextHour; index < nextHour + 3; index++) {
-//                 nextHours.push(index % 24);
-//             }
+            for (let index = nextHour; index < nextHour + 3; index++) {
+                nextHours.push(index % 24);
+            }
 
-//             const timeStrings = (nextHours.map(hour => minutes.map(minute => `${hour}:${minute}`))).flat();
-//             const [dayDate, month] = date.split(" ")[date.split(" ").length - 1].split(".");
+            const timeStrings = (nextHours.map(hour => minutes.map(minute => `${hour}:${minute}`))).reduce((flat, toFlatten) => (
+                flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten)
+            ), []);
+            const [dayDate, month] = date.split(" ")[date.split(" ").length - 1].split(".");
 
-//             rule.year = new Date().getFullYear();
-//             rule.month = Number(month) - 1;
-//             rule.date = Number(dayDate);
-//             rule.hour = hour;
-//             rule.minute = 0;
-//             rule.second = 0;
-//             rule.tz = "Asia/Jerusalem";
+            rule.year = new Date().getFullYear();
+            rule.month = Number(month) - 1;
+            rule.date = Number(dayDate);
+            rule.hour = hour;
+            rule.minute = 0;
+            rule.second = 0;
+            rule.tz = "Asia/Jerusalem";
 
-//             schedule.scheduleJob(rule, () => {
-//                 (async () => {
-//                     try {
-//                         const meetings = await app.models.meetings.find({ where: { and: [{ or: timeStrings.map(time => ({ time })) }, { date }] }, include: ["people", "meetingOwner", "fallens"] });
+            schedule.scheduleJob(rule, () => {
+                (async () => {
+                    try {
+                        const meetings = await app.models.meetings.find({ where: { and: [{ or: timeStrings.map(time => ({ time })) }, { date }] }, include: ["people", "meetingOwner", "fallens"] });
 
-//                         meetings.forEach(meeting => {
-//                             console.log(meeting);
-//                         });
-//                     } catch (err) {
-//                         console.error(err);
-//                     }
-//                 })();
-//             });
-//         });
-//     });
-// }
+                        meetings.forEach(meeting => {
+                            console.log(meeting);
+                        });
+                    } catch (err) {
+                        console.error(err);
+                    }
+                })();
+            });
+        });
+    });
+}
