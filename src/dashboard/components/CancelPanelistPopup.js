c3d57da014ac85122e95643976e4fc0cca7275c2
Your Name <reshariel@gmail.com>
2020-04-20 18:36:58 +0300

    set panelist status

---
+++ src/dashboard/components/CancelPanelistPopup.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import Auth from '../../modules/auth/Auth'
+
+export default function DaleteMeetingPopup(props) {
+
+    const [waitForData, setWaitForData] = useState(false)
+    const [err, setErr] = useState(false)
+
+    const deleteMeeting = async () => {
+        setWaitForData(true)
+        let [success, err] = await Auth.superAuthFetch(
+            `/api/meetings/setPanelistStatus`,
+            {
+                method: 'POST',
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                body: JSON.stringify({ meetingId: Number(props.meetingId), participantId: Number(props.currentParticipant.id), isPanelist: false })
+            }, true);
+        setWaitForData(false)
+        if (err) {
+            setErr(true)
+            return
+        }
+        props.setPanelistInArr(props.currentParticipant.id)
+        props.handleClose()
+    }
+
+    return (
+        <div>
+            <Dialog open={true} aria-labelledby="reset-modal">
+                <div style={{ padding: ' 2vh 3vw', width: '35vw' }}>
+                    <DialogContent>
+                        <p className="text-center" style={{
+                            fontSize: '2.9vh',
+                            color: '#A5A4BF',
+                            width: '100%'
+                        }}>
+                            <b>ביטול מנחה מפגש</b>
+                        </p>
+                        <p className="text-center" style={{
+                            color: '#A5A4BF',
+                            fontSize: '2.5vh',
+                            direction: 'rtl'
+                        }}>
+                            האם את בטוח שתרצה להסיר את <br />
+                            <b>{props.currentParticipant.name}</b><br />
+                            מהנחיית המפגש?
+                        </p>
+                        {err && <div>לא הצלחנו למחוק את המפגש. נסה שנית מאוחר יותר.</div>}
+                    </DialogContent>
+                    <DialogActions>
+                        <div className='d-flex' style={{ width: '100%' }}>
+                            <div
+                                style={{ cursor: 'pointer', backgroundColor: 'var(--custom-dark-green)', padding: '3px 3vw', borderRadius: '10px', color: 'white', fontSize: '2.5vh' }}
+                                onClick={() => {
+                                    if (waitForData) return
+                                    deleteMeeting()
+                                }}>
+                                {waitForData ?
+                                    <div className="spinner">
+                                        <div className="bounce1"></div>
+                                        <div className="bounce2"></div>
+                                        <div className="bounce3"></div>
+                                    </div>
+                                    : "כן"
+                                }
+                                {/* {this.props.t("approval")} */}
+                            </div>
+                            <div
+                                style={{ cursor: 'pointer', color: 'var(--custom-dark-green)', padding: '3px 10px', borderRadius: '10px', fontSize: '2.5vh', marginLeft: '2vw' }}
+                                onClick={() => {
+                                    props.handleClose()
+                                }}>
+                                ביטול
+                            </div>
+                        </div>
+                    </DialogActions>
+                </div>
+            </Dialog>
+        </div>
+    );
+}
