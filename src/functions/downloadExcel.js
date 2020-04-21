87c9c7109d7ed5bc91067361b4dd1b25b73c2f90
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-04-21 13:56:30 +0300

    file stuff

--- src/functions/downloadExcel.js
+++ src/functions/downloadExcel.js
@@ -1,7 +1,24 @@
 import { downloadFile, creatCsvFile } from "download-csv"
 
-const downloadExcel = (datas, columns, name) => {
-    downloadFile(creatCsvFile(datas, columns), name + '.xlsx');
+// example:
+// const data = [
+//     { name: "yona", phone: "7282" },
+//     { name: "mikmek", phone: "827627" },
+// ];
+
+// const columns = {
+//     name: "שם",
+//     phone: "טלפון"
+// }
+
+// downloadExcel(data, columns, "names-phones") =>
+// names-phones.xlsx
+// טלפון         שם
+// yona       7282
+// mikmek     827627
+
+const downloadExcel = (data, columns, name) => {
+    downloadFile(creatCsvFile(data, columns), name + '.xlsx');
 };
 
 export default downloadExcel;
