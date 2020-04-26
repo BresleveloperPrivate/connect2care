ba3b60d24a55611c4cab8da925bef40e6c175fe4
maayanLCohen <maayan45633@gmail.com>
2020-04-26 08:02:58 +0300

    send zoom host

---
+++ server/removePanelists.js
@@ -0,0 +1,41 @@
+// const jwt = require('jsonwebtoken');
+// const http = require("https");
+
+// const removePanelists = async (email, name, webinarId) => {
+
+//     const payload = {
+//         iss: "bxkoUl94RgOEagOunvJnDA",
+//         exp: ((new Date()).getTime() + 4000)
+//     };
+//     const token = jwt.sign(payload, "KOp8KDqjqW8wuAsi37VWUGnN61KJt7N8Enzy");
+
+
+//     let options = {
+//         "method": "POST",
+//         "hostname": "api.zoom.us",
+//         "port": null,
+//         "path": `/v2/webinars/${webinarId}/panelists/${email}?access_token=` + token,
+//         "headers": {
+//             "content-type": "application/json"
+//         }
+//     };
+
+//     let req = http.request(options, function (res) {
+//         let chunks = [];
+
+//         res.on("data", function (chunk) {
+//             chunks.push(chunk);
+//         });
+
+//         res.on("end", function () {
+//             let body = Buffer.concat(chunks);
+//             console.log(JSON.parse(body.toString()))
+//         });
+//     });
+
+
+//     req.end();
+
+// }
+
+// module.exports = removePanelists;
