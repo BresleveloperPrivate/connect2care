ccf558ccd22181f87cfd10e0a52ee4c45fc9cecc
admin <ariel.rubi@gmail.com>
2021-02-08 09:38:59 +0200

    new dump

--- dumps/create user.sql
+++ dumps/create user.sql
@@ -1,3 +1,23 @@
 FLUSH PRIVILEGES;
 CREATE USER 'ourBroDev'@'localhost' IDENTIFIED BY 'z10mz10m';
 GRANT ALL PRIVILEGES ON ourBro.* TO 'ourBroDev'@'localhost';
+
+
+
+
+
+/**** example problem query ****/
+SELECT meetings.id  
+FROM meetings  
+WHERE meetings.approved = 1  
+GROUP BY CASE 
+	WHEN meetings.isOpen = 1 AND meetings.participants_num < meetings.max_participants 
+		THEN 1        
+	WHEN meetings.isOpen = 0 AND meetings.participants_num < meetings.max_participants 
+		THEN 2
+	WHEN meetings.isOpen = 1 AND meetings.participants_num >= meetings.max_participants 
+		THEN 3 
+	WHEN meetings.isOpen = 0 AND meetings.participants_num >= meetings.max_participants 
+		THEN 4 
+	ELSE 5
+END , meetings.id DESC LIMIT 0 , 21
