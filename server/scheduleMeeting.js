5644e9b2014a2fc46e50f3a4f8e068d95bfd38a2
klior <karidi.lior@gmail.com>
2021-03-02 21:57:56 +0200

    update payload to zoom meetings api

--- server/scheduleMeeting.js
+++ server/scheduleMeeting.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 const http = require("https");
-const { log } = require('console');
+// const { log } = require('console');
 const {hostname, token} = require('./zoomAPIUtils');
 
 /**
@@ -36,38 +36,37 @@ const scheduleMeeting = async (cb, mail, start_time, pwd = Math.floor(Math.rando
 
         res.on("end", function () {
             let body = Buffer.concat(chunks);
-            // console.log(JSON.parse(body.toString()))
             let jsdata = JSON.parse(body.toString())
-            // console.log(jsdata)
-            if (jsdata.code === 1001) cb(null, jsdata)
-            else cb(jsdata.join_url, null)
+            if (jsdata.code === 1001) {
+                cb(null, jsdata)
+            } else {
+                cb(jsdata.join_url, null)
+            }
         });
     });
-
 
     // TODO verify meeting params
     req.write(JSON.stringify(
         {
-            "topic": "Connect 2 care Meeting",
-            "type": 5,
-            // "start_time": "2020-09-20T20:00:00",
+            "topic": "Connect 2 Care Meeting",
+            "type": 2,                      //  Scheduled meeting
             "timezone": "Asia/Jerusalem",
-            "start_time": start_time,
-            "duration": "240",
+            "start_time": start_time,       // format examples: GMT: 2020-03-31T12:02:00Z OR local (need timezone param): 2020-03-31T12:02:00
+            "duration": "240",              // in minutes
             "password": pwd,
-            "agenda": "Connect 2 care Meeting",
+            "agenda": "Connect 2 Care Meeting",
             "settings": {
-                "host_video": "true",
-                "panelists_video": "true",
-                "practice_session": "false",
-                "hd_video": "true",
-                "approval_type": 0,
-                "audio": "both",
-                "auto_recording": "cloud",
-                "enforce_login": "false",
-                "close_registration": "true",
-                "show_share_button": "true",
-                "allow_multiple_devices": "true"
+                "host_video": true,         // Start video when the host joins the meeting.
+                "participant_video": true,  // Start video when participants join the meeting.
+                "join_before_host": false,  // Allow participants to join the meeting before the host starts the meeting.
+                "mute_upon_entry": true,    // Mute participants upon entry.
+                "approval_type": 0,         // 0 - registration + Automatically approved. 1 - registration + Manually approve. 2 - No registration required (default)
+                "audio": "both",            // Both Telephony and VoIP. Determine how participants can join the audio portion of the meeting.
+                "auto_recording": "cloud",  // Automatic recording
+                "close_registration": true  // Close registration after event date
+                // "waiting_room": false,
+                // "show_share_button": true, // If set to true, the registration page for the meeting will include social share buttons. only applied for meetings that have enabled registration.
+                // "allow_multiple_devices": true, // If set totrue, attendees will be allowed to join a meeting from multiple devices. only applied for meetings that have enabled registration.
             }
         }
     ));
